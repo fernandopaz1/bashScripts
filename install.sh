68539da149f5529c739946878fa2c1cf1e82ea8c
Fernando Paz <ferpaz9292@gmail.com>
2020-09-08 07:30:52 -0300

    agrego aliases a los comandos de git

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 chmod 744 webPageTemplate.sh
 
-aliasWeb="alias webTemplate=/home/$USER/bashScripts/webPageTemplate.sh" 
-echo $aliasWeb>>/home/$USER/.bashrc
+aliasWeb="alias webTemplate=/home/$USER/bashScripts/webPageTemplate.sh"; 
+bashrcDir="/home/$USER/.bashrc";
+
+echo "">>$bashrcDir;
+
+
+echo $aliasWeb>>$bashrcDir;
+
+echo "">>$bashrcDir;
+
+cat gitAlias.txt>>$bashrcDir;
+
